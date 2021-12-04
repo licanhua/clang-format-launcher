6d136107ec1e3c6a76620d64d674fc9c0cd989a2
Canhua Li <licanhua@live.com>
2021-12-04 10:51:47 -0800

    update

--- src/index.ts
+++ src/index.ts
@@ -26,24 +26,48 @@ let excludePathContains: string[] = [];
 let excludePathEndsWith = ['.g.h', '.g.cpp'];
 let excludePathStartsWith: string[] = [];
 let folder = path.resolve(__dirname, '../../../');
+let style = "--style=file";
+
 let verbose = false;
 
 const VERIFY_FLAG = '-verify';
 const CONFIG_FILE = 'clang.format.json'
-const VERBOSE_FLAG = '-verbose';
+const VERBOSE_FLAG = '--verbose';
+const RAW_FLAG = '-raw';
+const HELP_FLAG = '--help';
 
 function main() {
   const verify = process.argv.indexOf(VERIFY_FLAG) > 0;
   verbose = process.argv.indexOf(VERBOSE_FLAG) > 0;
+  const raw = process.argv.indexOf(RAW_FLAG) > 0;
+  const help = process.argv.indexOf(HELP_FLAG) > 0;
+  if (help) {
+    printHelp();
+  }
 
-  const args = process.argv.slice(2).filter((_) => _ !== VERIFY_FLAG && _ !== VERBOSE_FLAG);
+  let args = process.argv.slice(2).filter((_) => _ !== VERIFY_FLAG && _ !== RAW_FLAG);
 
+  if (!raw) {
+    loadConfig();
 
-  loadConfig();
+    if (verify) {
+      args = ["-Werror", "--dry-run", ...args];
+    } else {
+      args = ["-Werror", "-i", ...args];
+    }
+    if (style) {
+      args = [style, ...args];
+    }
+  }
+
   // Run clang-format.
   try {
     // Pass all arguments to clang-format, including e.g. -version etc.
-    spawnClangFormat(args, verify ? queryNoOpenFiles : process.exit, 'inherit');
+    if (raw) {
+      spawnClangFormat(args, process.exit, 'inherit');
+    } else {
+      spawnClangFormat(args, verify ? queryNoOpenFiles : process.exit, 'inherit');
+    }
   } catch (e) {
     process.stdout.write((e as Error).message);
     process.exit(1);
@@ -56,6 +80,7 @@ interface Config {
   excludePathEndsWith?: string[],
   excludePathStartsWith?: string[],
   folder?: string,
+  style?: string,
 };
 
 function verboseLog(s: string) {
@@ -77,6 +102,7 @@ function loadConfig() {
       excludePathEndsWith = config.excludePathEndsWith ?? excludePathEndsWith;
       excludePathStartsWith = config.excludePathStartsWith ?? excludePathStartsWith;
       folder = config.folder ?? folder;
+      style = config.style ?? style;
       if (!path.isAbsolute(folder)) {
         folder = path.resolve(__dirname, '../../..', folder);
       }
@@ -96,6 +122,7 @@ function loadConfig() {
   verboseLog('  "excludePathEndsWith": ' + JSON.stringify(excludePathEndsWith));
   verboseLog('  "excludePathStartsWith": ' + JSON.stringify(excludePathStartsWith));
   verboseLog('  "folder": ' + folder);
+  verboseLog('  "style:' + JSON.stringify(style))
 }
 
 function queryNoOpenFiles() {
@@ -103,7 +130,6 @@ function queryNoOpenFiles() {
   if (opened) {
     console.error('The following files have incorrect formatting or not committed:');
     console.error(opened);
-    console.error('Running `format` from the repo root should fix this.');
     process.exit(2);
   }
 }
@@ -170,7 +196,7 @@ function spawnClangFormat(
     (file) =>
       includeEndsWith.some((_) => file.endsWith(_)) &&
       !excludePathContains.some((_) => file.indexOf(_) > 0) &&
-      !excludePathEndsWith.some((_) => file.endsWith(_)) && 
+      !excludePathEndsWith.some((_) => file.endsWith(_)) &&
       !excludePathStartsWith.some((_) => file.startsWith(_)) ,
   );
 
@@ -188,7 +214,6 @@ function spawnClangFormat(
   async.series<number, Error>(
     chunks.map((chunk) => {
       return function (callback) {
-        verboseLog("clang-format " + JSON.stringify(chunk));
         const clangFormatProcess = spawn(nativeBinary, args.concat(chunk), {
           cwd: folder,
           stdio: stdio,
@@ -215,6 +240,73 @@ function spawnClangFormat(
       done();
     },
   );
+}
+
+/**
+ * Spawn the clang-format binary with given arguments.
+ */
+function spawnClangFormatRaw(
+  args: string[],
+  done: (any?: any) => void,
+  stdio: StdioOptions,
+) {
+  // WARNING: This function's interface should stay stable across versions for the cross-version
+  // loading below to work.
+  let nativeBinary: string;
+
+  try {
+    nativeBinary = getNativeBinary();
+    verboseLog("native Clang-format: " + nativeBinary);
+  } catch (e) {
+    setImmediate(done.bind(e));
+    return;
+  }
+
+  verboseLog("clang-format " + JSON.stringify(args));
+  const clangFormatProcess = spawn(nativeBinary, args, {
+    cwd: folder,
+    stdio: stdio,
+  });
+  clangFormatProcess.on('close', (exit) => {
+    if (exit !== 0) {
+      errorFromExitCode(exit!);
+    }
+  }
+  );
+}
+
+function printHelp() {
+  console.log(
+    `
+clang-format-launcher is an clang-format wrapper.
+It uses 'git ls-tree' to speed up the file lookup, then filters the files by the rule which is defined in clang.format.json.
+Usage:
+  npx clang-format-launcher [options] [other options]
+    Options:
+      -raw
+      -verify 
+
+  npx clang-format-launcher [other options]
+    equal to 'npx clang-format --style=file -Werror -i [other options] [Files after filter]'
+
+  npx clang-format-launcher -verify [other options]
+    equal to 'npx clang-format --style=file -Werror "--dry-run" [other options] [Files after filter]'
+
+  npx clang-format-launcher -raw [other options]
+    equal to 'npx clang-format  [other options]
+
+clang.format.json example:
+{
+  "includeEndsWith": [".h",".cpp"],
+  "excludePathContains": ["/ios/", "/nodejs/", "/android/"],
+  "excludePathEndsWith": [".g.h",".g.cpp"],  
+  "excludePathStartsWith": [],
+  "folder": "../..",
+  "style": "--style=file"
+}
+    
+`
+  )
 }
 
 main();
